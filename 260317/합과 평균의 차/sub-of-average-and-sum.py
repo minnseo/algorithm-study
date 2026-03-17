447135d0761c86ca38c01ac89adc968bf8113bef
minnseo <106568461+minnseo@users.noreply.github.com>
2026-03-17 10:31:21 +0900

    Title: 합과 평균의 차, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 260317/합과 평균의 차/sub-of-average-and-sum.py
+++ 260317/합과 평균의 차/sub-of-average-and-sum.py
@@ -1,9 +1,5 @@
 a, b, c = map(int, input().split())
 
 s = a + b + c
-average = (a + b + c) // 3
-diff = s - average
 
-print(s)
-print(average)
-print(diff)
+print(s, s // 3, s - (s // 3), sep = '\n')
