907ae19921c1cb90cf2164a20588cf45566dd826
minnseo <106568461+minnseo@users.noreply.github.com>
2026-03-18 14:51:35 +0900

    Title: 비교 연산, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 260318/비교 연산/comparison-operator.py
+++ 260318/비교 연산/comparison-operator.py
@@ -1,31 +1,8 @@
-a, b = map(int, input().split())
+A, B = map(int, input().split())
 
-if a >= b:
-    print(1)
-else:
-    print(0)
-
-if a > b:
-    print(1)
-else:
-    print(0)
-
-if b >= a:
-    print(1)
-else:
-    print(0)
-
-if b > a:
-    print(1)
-else:
-    print(0)
-
-if a == b:
-    print(1)
-else:
-    print(0)
-
-if a != b:
-    print(1)
-else:
-    print(0)
+print(1 if A >= B else 0)
+print(1 if A > B else 0)
+print(1 if B >= A else 0)
+print(1 if B > A else 0)
+print(1 if A == B else 0)
+print(1 if A != B else 0)
