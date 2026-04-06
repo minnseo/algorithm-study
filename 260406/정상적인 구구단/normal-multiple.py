ea49b92792f61dc46d4c05a2ea23634369a06cd4
minnseo <106568461+minnseo@users.noreply.github.com>
2026-04-06 15:36:26 +0900

    Title: 정상적인 구구단, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260406/정상적인 구구단/normal-multiple.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        print(f'{i} * {j} = {i * j}', end = '')
+
+        if j != n:
+            print(',', end = ' ')
+    
+    print()
