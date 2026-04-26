cf620cf5a9374a1057d168b77da03c1cf4b5a2bb
minnseo <106568461+minnseo@users.noreply.github.com>
2026-04-26 23:14:02 +0900

    Title: 날짜 변경하여 출력 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260426/날짜 변경하여 출력 2/print-date-with-different-format-2.py
+++ 260426/날짜 변경하여 출력 2/print-date-with-different-format-2.py
@@ -1,4 +1,3 @@
-month, date, year = map(int, input().split('-'))
+m, d, y = map(int, input().split('-'))
 
-if 1 <= year <= 2021 and 1 <= month <= 12 and 1 <= date <= 28: 
-    print(year, month, date, sep = '.')
+print(y, m, d, sep = '.')
